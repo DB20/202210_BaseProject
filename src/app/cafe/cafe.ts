77b158f6f27b0de3dc6ca5d3bd69b1fe9bfdfce8
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2022-05-01 10:46:58 -0500

    Clase Café agregada

--- src/app/cafe/cafe.ts
+++ src/app/cafe/cafe.ts
@@ -1,3 +1,19 @@
 export class Cafe {
+  id: Number;
+  nombre: String;
+  tipo: String;
+  region: String;
+  sabor: String;
+  altura: String;
+  image: String;
 
+  public constructor(id: number, nombre: string, tipo: string, region: string,sabor:string, altura:string,image: string) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.region = region;
+    this.sabor = sabor;
+    this.altura = altura;
+    this.image = image;
+ }
 }
