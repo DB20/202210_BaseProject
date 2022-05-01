10edf6ba6f5a79da15f559a6b118628c57bb1a8d
Krishtian Barrera <kd.barrera@uniandes.edu.co>
2022-05-01 13:22:37 -0500

    test pruebas

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Cafe } from '../cafe';
+import { CafeService} from '../cafe.service';
+import { System } from '@faker-js/faker/system';
+
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CafeListComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -19,10 +28,46 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+    ];
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it("Component has a table with 4 rows, the header and three coffes", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toEqual(4);
+  });
+
 });
